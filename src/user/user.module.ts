751d1a10bc770f4499c7232aa56f561041f8d5bc
Muhammed Rashid N <85437745+rashidpkn@users.noreply.github.com>
2023-05-17 17:36:06 +0530

    Somthing is added

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  controllers: [UserController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../build'),
+    }),
+  ],
+  controllers: [UserController,],
   providers: [UserService]
 })
 export class UserModule {}
